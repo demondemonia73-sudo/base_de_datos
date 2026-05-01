275b4556365e2a67d478e3d10fbe1b893bac9156
demondemonia73-sudo <demondemonia73@gmail.com>
2026-05-01 17:51:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+// =====================================================
+// CONFIGURACIÓN DE SUPABASE
+// =====================================================
 
+const SUPABASE_URL = 'https://inghjpgqahmceplasunr.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_-_MBMDKrs8qfUlwscp9phQ_V9w1TjYv';
